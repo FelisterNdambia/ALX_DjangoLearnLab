62a54e07ba18a09f1f78183df68418714b9bc023
Felister Ndambia <ndambiafelister914@gmailcom>
2025-09-13 17:28:32 +0300

    Completed bookshelf app with Book model and CRUD documentation

--- Introduction_to_Django/LibraryProject/manage.py
+++ Introduction_to_Django/LibraryProject/manage.py
@@ -2,11 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-sys.path.append(r'C:\Users\dell\Desktop\Introduction_to_Django\LibraryProject')
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +18,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

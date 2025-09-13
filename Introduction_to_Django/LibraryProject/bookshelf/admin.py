06862ab8cccaab5e66a3c0b2dc7aefc4f5c336a9
Felister Ndambia <ndambiafelister914@gmailcom>
2025-09-13 18:24:04 +0300

    Add Django admin setup and update CRUD markdown files

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # columns in list view
+    list_filter = ('publication_year',)                     # add filter sidebar
+    search_fields = ('title', 'author')                    # enable search box
 
 # Register your models here.

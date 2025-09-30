fd0eb601cf5108e16ba3017b0c4286343875245f
Felister Ndambia <ndambiafelister914@gmailcom>
2025-09-30 21:49:02 +0300

    Add ExampleForm, secure views, permissions, and update README

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required, login_required
 from .models import Book
-from .forms import BookForm
+from .forms import ExampleForm
 
 # View to list all books with optional search
 @login_required
